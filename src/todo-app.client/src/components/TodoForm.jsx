b633aef86c5eec01cdd50accbf2ab7e27e9f9d8f
Andres Hernandez <andhernand@gmail.com>
2024-06-18 15:13:01 -0600

    Use SVGs for button icons
    I got tired of seeing the webfonts pop in during page load.
    
    also cleans up some unneeded html in the todo form.

--- src/todo-app.client/src/components/TodoForm.jsx
+++ src/todo-app.client/src/components/TodoForm.jsx
@@ -22,22 +22,23 @@ const TodoForm = ({ dispatch }) => {
   };
 
   return (
-    <div>
-      <form id="add-task-form" onSubmit={(e) => handleSubmit(e)}>
-        <div className="form">
-          <input
-            id="task-input"
-            type="text"
-            ref={description}
-            className="form-input"
-            placeholder="Add a task"
-          />
-          <button type="submit" className="button icon-button">
-            <i className="fa-solid fa-plus"></i>
-          </button>
-        </div>
-      </form>
-    </div>
+    <form id="add-task-form" onSubmit={(e) => handleSubmit(e)}>
+      <div className="form">
+        <input
+          id="task-input"
+          type="text"
+          ref={description}
+          className="form-input"
+          placeholder="Add a task"
+        />
+        <button type="submit" className="button">
+          <svg className="svg-icon-button" viewBox="0 0 448 512">
+            <title>submit</title>
+            <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+          </svg>
+        </button>
+      </div>
+    </form>
   );
 };
 
